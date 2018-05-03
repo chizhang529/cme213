8896056cdcf475e9161e0ac9fc4c98564c8cbdfc
chizhang529 <czhang94@stanford.edu>
2018-05-02 21:42:16 -0700

    Finished HW2 Q2

--- HW2/hw2/pagerank.cu
+++ HW2/hw2/pagerank.cu
@@ -1,8 +1,20 @@
+// utility function for checking cuda errors
+#define checkCudaErrors(val) check( (val), #val, __FILE__, __LINE__)
+template<typename T>
+void check(T err, const char* const func, const char* const file, const int line) {
+  if (err != cudaSuccess) {
+    std::cerr << "CUDA error at: " << file << ":" << line << std::endl;
+    std::cerr << cudaGetErrorString(err) << " " << func << std::endl;
+    exit(1);
+  }
+}
+
+static const uint MAX_GRID_DIM = 65535;
 /* Each kernel handles the update of one pagerank score. In other
  * words, each kernel handles one row of the update:
  *
  *      pi(t+1) = (1/2) A pi(t) + (1 / (2N))
- *      
+ *
  * You may assume that num_nodes <= blockDim.x * 65535
  *
  */
@@ -13,7 +25,29 @@ void device_graph_propagate(const uint* graph_indices
                             , float* graph_nodes_out
                             , const float* inv_edges_per_node
                             , int num_nodes) {
-    // TODO: fill in the kernel code here
+    uint bid = blockIdx.y * gridDim.x + blockIdx.x;
+    uint tid = bid * blockDim.x + threadIdx.x;
+    if (tid < num_nodes) {
+        float sum = 0.0f;
+        // iterate all edges
+        uint start = graph_indices[tid], end = graph_indices[tid+1];
+        for (uint i = start; i < end; ++i) {
+            uint node = graph_edges[i];
+            sum += 0.5f * inv_edges_per_node[node] * graph_nodes_in[node];
+        }
+        // add constant term
+        sum += 1.0f / (2 * num_nodes);
+        // update
+        graph_nodes_out[tid] = sum;
+    }
+}
+
+// swap pointers to input and output array
+void swapio(float **in, float **out) {
+    float *temp;
+    temp = *in;
+    *in = *out;
+    *out = temp;
 }
 
 /* This function executes a specified number of iterations of the
@@ -53,24 +87,69 @@ double device_graph_iterate(const uint* h_graph_indices
                             , int nr_iterations
                             , int num_nodes
                             , int avg_edges) {
-    // TODO: allocate GPU memory
+    // allocate GPU memory
+    const uint num_indices = num_nodes + 1;
+    const uint num_edges = num_nodes * avg_edges;
 
-    // TODO: check for allocation failure
+    uint *d_graph_indices, *d_graph_edges;
+    float *d_input, *d_output, *d_inv_edges_per_node;
 
-    // TODO: copy data to the GPU
+    cudaMalloc((void **)&d_graph_indices, num_indices * sizeof(uint));
+    cudaMalloc((void **)&d_graph_edges, num_edges * sizeof(uint));
+    cudaMalloc((void **)&d_input, num_nodes * sizeof(float));
+    cudaMalloc((void **)&d_output, num_nodes * sizeof(float));
+    cudaMalloc((void **)&d_inv_edges_per_node, num_nodes * sizeof(float));
+
+    // check for allocation failure
+    cudaDeviceSynchronize();
+    checkCudaErrors(cudaGetLastError());
+    // copy data to the GPU
+    cudaMemcpy(d_graph_indices, h_graph_indices, num_indices * sizeof(uint), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_graph_edges, h_graph_edges, num_edges * sizeof(uint), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_input, h_node_values_input, num_nodes * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_inv_edges_per_node, h_inv_edges_per_node,
+               num_nodes * sizeof(float), cudaMemcpyHostToDevice);
 
     start_timer(&timer);
 
+    // compute grid dimensions
     const int block_size = 192;
+    // compute grid dimensions
+    dim3 blocks_per_grid(1, 1);         // 2D grid
+    dim3 threads_per_block(block_size); // 1D block
+    // compute number of blocks needed
+    uint num_blocks = ceil((float)num_nodes / (float)block_size);
+    if (num_blocks > MAX_GRID_DIM) {
+        blocks_per_grid.x = MAX_GRID_DIM;
+        blocks_per_grid.y = ceil((float)num_blocks / (float)MAX_GRID_DIM);
+    } else {
+        blocks_per_grid.x = num_blocks;
+    }
 
-    // TODO: launch your kernels the appropriate number of iterations
+    // launch kernel the appropriate number of iterations
+    for (int i = 0; i < nr_iterations; ++i) {
+        device_graph_propagate<<<blocks_per_grid, threads_per_block>>>(d_graph_indices,
+                                                                       d_graph_edges,
+                                                                       d_input, d_output,
+                                                                       d_inv_edges_per_node,
+                                                                       num_nodes);
+        swapio(&d_input, &d_output);
+    }
 
     check_launch("gpu graph propagate");
     double gpu_elapsed_time = stop_timer(&timer);
 
-    // TODO: copy final data back to the host for correctness checking
+    // copy final data back to the host for correctness checking
+    cudaMemcpy(h_gpu_node_values_output, d_input, num_nodes * sizeof(float), cudaMemcpyDeviceToHost);
 
-    // TODO: free the memory you allocated!
-
+    // free memory
+    cudaFree(d_graph_indices);
+    cudaFree(d_graph_edges);
+    cudaFree(d_input);
+    cudaFree(d_output);
+    cudaFree(d_inv_edges_per_node);
+    // error checking
+    cudaDeviceSynchronize();
+    checkCudaErrors(cudaGetLastError());
     return gpu_elapsed_time;
 }
--- HW2/hw2/shift.cu
+++ HW2/hw2/shift.cu
@@ -7,12 +7,16 @@
 
 typedef unsigned char uchar;
 typedef unsigned int uint;
+static const uint MAX_GRID_DIM = 65535;
 
 // This kernel implements a per element shift
 // by naively loading one byte and shifting it
 __global__ void shift_char(const uchar* input_array, uchar* output_array,
                            uchar shift_amount, uint array_length) {
-    // TODO: fill in
+    uint bid = blockIdx.y * gridDim.x + blockIdx.x;
+    uint tid = bid * blockDim.x + threadIdx.x;
+    if (tid < array_length)
+        output_array[tid] = input_array[tid] + shift_amount;
 }
 
 //Here we load 4 bytes at a time instead of just 1
@@ -20,57 +24,103 @@ __global__ void shift_char(const uchar* input_array, uchar* output_array,
 //access pattern
 __global__ void shift_int(const uint* input_array, uint* output_array,
                           uint shift_amount, uint array_length) {
-    // TODO: fill in
+    uint bid = blockIdx.y * gridDim.x + blockIdx.x;
+    uint tid = bid * blockDim.x + threadIdx.x;
+    if (tid < array_length)
+        output_array[tid] = input_array[tid] + shift_amount;
 }
 
 //Here we go even further and load 8 bytes
 //does it make a further improvement?
 __global__ void shift_int2(const uint2* input_array, uint2* output_array,
                            uint shift_amount, uint array_length) {
-    // TODO: fill in
+    uint bid = blockIdx.y * gridDim.x + blockIdx.x;
+    uint tid = bid * blockDim.x + threadIdx.x;
+    if (tid < array_length) {
+        output_array[tid].x = input_array[tid].x + shift_amount;
+        output_array[tid].y = input_array[tid].y + shift_amount;
+    }
 }
 
 //the following three kernels launch their respective kernels
 //and report the time it took for the kernel to run
-
 double doGPUShiftChar(const uchar* d_input, uchar* d_output,
                       uchar shift_amount, uint text_size, uint block_size) {
-    // TODO: compute your grid dimensions
+    // compute grid dimensions (NOTE: block size is 256 in tests)
+    dim3 blocks_per_grid(1, 1);         // 2D grid
+    dim3 threads_per_block(block_size); // 1D block
+    // compute number of blocks needed
+    uint num_blocks = ceil((float)text_size / (float)block_size);
+    if (num_blocks > MAX_GRID_DIM) {
+        blocks_per_grid.x = MAX_GRID_DIM;
+        blocks_per_grid.y = ceil((float)num_blocks / (float)MAX_GRID_DIM);
+    } else {
+        blocks_per_grid.x = num_blocks;
+    }
 
     event_pair timer;
     start_timer(&timer);
 
-    // TODO: launch kernel
-
+    // launch kernel
+    shift_char<<<blocks_per_grid, threads_per_block>>>(d_input, d_output, shift_amount, text_size);
     check_launch("gpu shift cipher char");
     return stop_timer(&timer);
 }
 
 double doGPUShiftUInt(const uchar* d_input, uchar* d_output,
                       uchar shift_amount, uint text_size, uint block_size) {
-    // TODO: compute your grid dimensions
-
-    // TODO: compute 4 byte shift value
+    // compute grid dimensions
+    dim3 blocks_per_grid(1, 1);         // 2D grid
+    dim3 threads_per_block(block_size); // 1D block
+    // compute number of blocks needed
+    uint num_blocks = ceil((float)text_size / (float)block_size);
+    uint scale = sizeof(uint) / sizeof(uchar);
+    num_blocks = ceil((float)num_blocks / (float)scale);
+    if (num_blocks > MAX_GRID_DIM) {
+        blocks_per_grid.x = MAX_GRID_DIM;
+        blocks_per_grid.y = ceil((float)num_blocks / (float)MAX_GRID_DIM);
+    } else {
+        blocks_per_grid.x = num_blocks;
+    }
+    // compute 4 byte shift value
+    uint shift = shift_amount | (shift_amount << 8) | (shift_amount << 16) | (shift_amount << 24);
 
     event_pair timer;
     start_timer(&timer);
 
-    // TODO: launch kernel
-
+    // launch kernel
+    uint array_length = ceil((float)text_size / (float)scale);
+    shift_int<<<blocks_per_grid, threads_per_block>>>((const uint *)d_input, (uint *)d_output,
+                                                       shift, array_length);
     check_launch("gpu shift cipher uint");
     return stop_timer(&timer);
 }
 
 double doGPUShiftUInt2(const uchar* d_input, uchar* d_output
                        , uchar shift_amount, uint text_size, uint block_size) {
-    // TODO: compute your grid dimensions
-
-    // TODO: compute 4 byte shift value
+    // compute grid dimensions
+    dim3 blocks_per_grid(1, 1);         // 2D grid
+    dim3 threads_per_block(block_size); // 1D block
+    // compute number of blocks needed
+    uint num_blocks = ceil((float)text_size / (float)block_size);
+    uint scale = sizeof(uint2) / sizeof(uchar);
+    num_blocks = ceil((float)num_blocks / (float)scale);
+    if (num_blocks > MAX_GRID_DIM) {
+        blocks_per_grid.x = MAX_GRID_DIM;
+        blocks_per_grid.y = ceil((float)num_blocks / (float)MAX_GRID_DIM);
+    } else {
+        blocks_per_grid.x = num_blocks;
+    }
+    // compute 4 byte shift value
+    uint shift = shift_amount | (shift_amount << 8) | (shift_amount << 16) | (shift_amount << 24);
 
     event_pair timer;
     start_timer(&timer);
 
-    // TODO: launch kernel
+    // launch kernel
+    uint array_length = ceil((float)text_size / (float)scale);
+    shift_int2<<<blocks_per_grid, threads_per_block>>>((const uint2 *)d_input, (uint2 *)d_output,
+                                                       shift, array_length);
 
     check_launch("gpu shift cipher uint2");
     return stop_timer(&timer);
